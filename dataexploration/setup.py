d397ad8884b4c51208a7cc44e2d440ff22faefb1
Daphne <50707529+daphn3k@users.noreply.github.com>
2019-07-20 23:01:37 +0200

    Update setup.py

--- dataexploration/setup.py
+++ dataexploration/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='dataexploration',
       version='0.1',
       description='Exploring NaN values in pandas DataFrame',
-      url='http://github.com/daphn3k/data_science_module',
+      url='http://github.com/daphn3k/data_analysis',
       author='Daphne',
       license='GNU GLP v3.0',
       packages=['dataexploration'],
