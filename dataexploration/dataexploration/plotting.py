4123966b440701a4d9729786ebc1823c37958187
Daphne <50707529+daphn3k@users.noreply.github.com>
2019-09-02 14:27:52 +0200

    numeric_Hist now saves to file

--- dataexploration/dataexploration/plotting.py
+++ dataexploration/dataexploration/plotting.py
@@ -2,43 +2,65 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd 
 
-def numeric_Hist(df, plotCols = 4, figsize = (18,18), kde = False, bins = None, color = None):
+def numeric_Hist(df, plotCols = 4, plotRows = 4, figsize = (18,18), kde = False, bins = None, color = None, path=''):
 
-    '''Plot histograms of the numerical columns of a pandas.Dataframe
+    '''Plot group of histograms of the numerical columns of a pandas.Dataframe - NaN values are ignored.
+    Plots are saved to .png file(s)
     
      Parameters:
     -----------
     - df {pandas.Dataframe}: the dataframe to be plotted
 
-    Optional arugments:
-    - plotCols {int}: number of columns the histogram will have. [Default value: 4]
+    Optional arguments:
+    - plotCols {int}: number of histograms per column. [Default value: 4]
+    - plotRows {int}: number of histograms per row. [Default value: 4]
     - figsize {tuple}: argument of matplotlib.pyplot.suplobts. [Default value: (18,18)]
     - kde {bool}: argument of seaborn.distplot. Whether to plot a gaussian kernel density estimate. [Default value: False] 
     - bins {int}: Specification of hist bins. [Default value: None - uses Freedman-Diaconis rule]
     - color {matplotlib color}: color to plot everything [Default value: None - output is blue]
+    - path {str}: optional. Path were to save the file(s) - if in diffent folder, then it must be already present in your system
+      [Default vaue: '' - will be saved at same folder with your script]
     
+     Outputs:
+    File(s) with histograms. Naming: hist_firstFeature-lastFeature.png
     '''
 
     # which columns are numerical and will be plotted
     nameCols = [el for el in df if df[el].dtypes != 'object']
 
     # how many rows should be present
-    if len(nameCols) <= plotCols:
-        plotRows = 1
-        plotCols = len(nameCols)
-    else: 
-        plotRows = -(-len(nameCols)//plotCols)
+    # if len(nameCols) <= plotCols:
+    #     plotRows = 1
+    #     plotCols = len(nameCols)
+    # else: 
+    #     plotRows = -(-len(nameCols)//plotCols)
 
-    # plotting
-    fig, axes = plt.subplots(plotRows, plotCols, figsize=figsize)
-    fig.suptitle('Distributions of columns with numeric values')
-    fig.subplots_adjust(hspace=0.5, wspace=0.3)
-    for el, ax in zip(nameCols, axes.flatten()):
-        sns.distplot(df[el].dropna(), hist_kws = {'edgecolor':'grey', 'linewidth':1}, ax= ax, kde = kde, bins = bins, color = color)
-        #ax.set(title=el)
-    plt.show()
+    i = 0 
+    j = 1 
+    k = plotRows*plotCols 
+    colsToPlot = [] 
+    while i<len(nameCols): 
+        colsToPlot.append(nameCols[i:j*k]) 
+        i += j*k 
+        j +=1 
+    
+    # plotting 
+    for toPlot in colsToPlot: 
+        fig, axes = plt.subplots(plotRows, plotCols, figsize=figsize)
+        fig.suptitle('Distributions of columns with numeric values')
+        fig.subplots_adjust(hspace=0.5, wspace=0.3)
+        for el, ax in zip(toPlot, axes.flatten()):
+            sns.distplot(df[el].dropna(), hist_kws = {'edgecolor':'grey', 'linewidth':1}, ax= ax, kde = kde, bins = bins, color = color)
+            #ax.set(title=el)
+        
+        # save file
+        fileName = path + 'hist' + '_' + toPlot[0].title() + '-' + toPlot[-1].title() + '.png'
+        plt.savefig(fileName, format='png')
+        #plt.show()
 
     return
+
+                                             
 
 ''' to do: 
 
